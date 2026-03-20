5f1b809bac72bd0e26fbd797e8ec9178827b0a17
haripriyaganil <haripriyaanil1234@gmail.com>
2026-03-21 00:33:41 +0530

    axios

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://librarymanagementsys-txvj.onrender.com/api",
+  baseURL: "https://librarymanagementdeploy.onrender.com/api",
 });
 
 export default api;
